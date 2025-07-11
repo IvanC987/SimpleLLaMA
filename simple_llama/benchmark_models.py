f1d27a34a345dbf3493b53916b0f0b67fa12f4c5
Ivan Cao <ivanc135246@gmail.com>
2025-07-10 23:32:45 -0500

    added arc-c eval and refactored some code

--- simple_llama/benchmark_models.py
+++ simple_llama/benchmark_models.py
@@ -85,6 +85,57 @@ def format_sample(question: str, choices: list[str]):
     return prefix_string, chosen
 
 
+def helper_function(model: LLaMaTransformer, tokenizer: Tokenizer, pad_tok_id: int, prefix_string: str, chosen: str,
+                    question: str, choices: list[str], answer: str, device: str):
+
+    prefix_tokens = tokenizer.encode(prefix_string).ids
+    chosen_tokens = [tokenizer.encode(e).ids for e in chosen]
+    max_seq_len = max([len(t) for t in chosen_tokens]) + len(prefix_tokens)
+    pad_count = []  # Keep track how many padding token each option got
+    concat_str = []  # This will be the final string of prefix question + answer tokens + padding
+    for toks in chosen_tokens:  # Need to pad accordingly
+        pc = max_seq_len - len(toks)
+        pad_count.append(pc)
+        concat_str.append(prefix_tokens + toks + [pad_tok_id] * pc)
+
+    # Shape (x, max_tok_len, vocab_size), x being number of choices (batches)
+    batch_tensor = torch.tensor(concat_str, dtype=torch.long, device=device)
+
+    try:
+        # Pass it through the model, should be same shape as input, left shifted
+        result = model(batch_tensor)
+    except Exception as e:
+        print(f"Model execution failed on sample with error: {e}\n"
+              f"{question=}\n"
+              f"{choices=}\n"
+              f"{answer=}\n")
+        return
+
+        # Slice off the question/choices part. Only retain answer portion
+    num_prefix_tokens = len(prefix_tokens)
+    result = result[:, num_prefix_tokens:, :]
+
+    # Now calculate the model's 'choice'
+    model_answer = []
+    for i, sequence in enumerate(result):
+        truncate_idx = len(sequence) - pad_count[i] - 1  # Additional -1 due to next-token-prediction objective?
+        sequence = sequence[:truncate_idx, :]  # Remove padding token
+        probs = F.softmax(sequence, dim=-1)
+
+        # Grab the target ids, need to convert probs.shape (seq_len, vocab_size) to (seq_len)
+        target_ids = chosen_tokens[i][1:]
+        target_ids = torch.tensor(target_ids, dtype=torch.long, device=device).unsqueeze(1)
+        log_probs = torch.log(probs + 1e-12)  # Numerical safety
+
+        # Gather the log-probs for the target tokens
+        target_log_probs = log_probs.gather(dim=1, index=target_ids).unsqueeze(1)
+
+        # Final log sum
+        model_answer.append(target_log_probs.sum().item())
+
+    return model_answer
+
+
 @torch.no_grad()
 def mmlu_eval(model: LLaMaTransformer, tokenizer: Tokenizer, max_chars: int, pad_tok_id: int):
     """
@@ -128,7 +179,7 @@ def mmlu_eval(model: LLaMaTransformer, tokenizer: Tokenizer, max_chars: int, pad
         else:
             normalized_dataset.append(sample)
 
-    print(f"Discarded ({count} samples) {100 * count / len(test_set):.2f}% discarded after ASCII filtering")
+    print(f"Discarded {count}/{len(test_set)} samples ({100 * count / len(test_set):.2f}%) after ASCII filtering")
 
     # Now iterating through the test samples
     final_result = []  # 1 for correct, 0 for incorrect
@@ -141,50 +192,20 @@ def mmlu_eval(model: LLaMaTransformer, tokenizer: Tokenizer, max_chars: int, pad
         # prefix_str is the prefix shared by all options
         prefix_string, chosen = format_sample(question, choices)
 
-        prefix_tokens = tokenizer.encode(prefix_string).ids
-        chosen_tokens = [tokenizer.encode(e).ids for e in chosen]
-        max_seq_len = max([len(t) for t in chosen_tokens]) + len(prefix_tokens)
-        pad_count = []  # Keep track how many padding token each option got
-        concat_str = []  # This will be the final string of prefix question + answer tokens + padding
-        for toks in chosen_tokens:  # Need to pad accordingly
-            pc = max_seq_len - len(toks)
-            pad_count.append(pc)
-            concat_str.append(prefix_tokens + toks + [pad_tok_id] * pc)
-
-        # Shape (x, max_tok_len, vocab_size), x being number of choices (batches)
-        batch_tensor = torch.tensor(concat_str, dtype=torch.long, device=device)
-
-        try:
-            # Pass it through the model, should be same shape as input, left shifted
-            result = model(batch_tensor)
-        except Exception as e:
-            print(f"Model execution failed on sample with error: {e}\n"
-                  f"{question=}\n"
-                  f"{choices=}\n"
-                  f"{answer=}\n")
-            continue  # skip to next sample, or handle differently
-
-        # Slice off the question/choices part. Only retain answer portion
-        num_prefix_tokens = len(prefix_tokens)
-        result = result[:, num_prefix_tokens:, :]
-
-        # Now calculate the model's 'choice'
-        model_answer = []
-        for i, sequence in enumerate(result):
-            truncate_idx = len(sequence) - pad_count[i] - 1  # Additional -1 due to next-token-prediction objective?
-            sequence = sequence[:truncate_idx, :]  # Remove padding token
-            probs = F.softmax(sequence, dim=-1)
-
-            # Grab the target ids, need to convert probs.shape (seq_len, vocab_size) to (seq_len)
-            target_ids = chosen_tokens[i][1:]
-            target_ids = torch.tensor(target_ids, dtype=torch.long, device=device).unsqueeze(1)
-            log_probs = torch.log(probs + 1e-12)  # Numerical safety
-
-            # Gather the log-probs for the target tokens
-            target_log_probs = log_probs.gather(dim=1, index=target_ids).unsqueeze(1)
-
-            # Final log sum
-            model_answer.append(target_log_probs.sum().item())
+        kwargs = {
+            "model": model,
+            "tokenizer": tokenizer,
+            "pad_tok_id": pad_tok_id,
+            "prefix_string": prefix_string,
+            "chosen": chosen,
+            "question": question,
+            "choices": choices,
+            "answer": answer,
+            "device": device
+        }
+        model_answer = helper_function(**kwargs)
+        if model_answer is None:
+            continue
 
         # Convert into tensor
         model_answer = torch.tensor(model_answer, dtype=torch.long, device=device)
@@ -199,15 +220,98 @@ def mmlu_eval(model: LLaMaTransformer, tokenizer: Tokenizer, max_chars: int, pad
 
 
 @torch.no_grad()
-def arc_c_eval():
-    pass
+def arc_c_eval(model: LLaMaTransformer, tokenizer: Tokenizer, max_chars: int, pad_tok_id: int):
+    """
+    It seems like there are a few questions that have 3/5 choices in the test set.
+    Vast majority (>99.9%) have 4 choices
+    """
+    arc_challenge = load_dataset("ai2_arc", "ARC-Challenge")
+
+    # Test set contains 1172 examples
+    test_set = arc_challenge["test"]
+
+    # Filter out examples with non-ascii characters with basic replacement
+    count = 0
+    normalized_dataset = []
+    for sample in test_set:
+        # Example sample format:
+        # {'id': 'Mercury_7175875',
+        #  'question': 'An astronomer observes that a planet rotates faster after a meteorite impact. Which is the most likely effect of this increase in rotation?',
+        #  'choices': {'text': ['Planetary density will decrease.', 'Planetary years will become longer.',
+        #                       'Planetary days will become shorter.', 'Planetary gravity will become stronger.'],
+        # 'label': ['A', 'B', 'C', 'D']},
+        # 'answerKey': 'C'}
+
+        sample["question"] = normalize_characters(sample["question"])
+
+
+        # Need to make sure they are all valid ASCII
+        if not sample["question"].isascii():
+            count += 1
+        elif not "".join(sample["choices"]["text"]).isascii():
+            count += 1
+        elif len(sample["question"] + "".join(sample["choices"]["text"])) > max_chars:
+            count += 1
+        elif sample["answerKey"] not in ['A', 'B', 'C', 'D', 'E']:
+            # Perhaps a bit questionable to remove, but over 99% of the test is among A-E as answers
+            # Keeping it consistent is better imo
+            count += 1
+        else:
+            normalized_dataset.append(sample)
+
+    print(f"Discarded {count}/{len(test_set)} samples ({100 * count / len(test_set):.2f}%) after ASCII filtering")
+
+    # Now iterating through the test samples
+    final_result = []  # 1 for correct, 0 for incorrect
+    mapping = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4}
+    for sample in tqdm(normalized_dataset):
+        question = sample["question"]  # A single string. E.g. "What color is the sky?"
+        choices = sample["choices"]["text"]  # A list of strings. E.g. ["blue", "red", "green"]
+        answer = mapping[sample["answerKey"]]  # An integer based on the mapped answer
+
+        # Options will now be a list of strings, formatted accordingly
+        # prefix_str is the prefix shared by all options
+        prefix_string, chosen = format_sample(question, choices)
+
+        kwargs = {
+            "model": model,
+            "tokenizer": tokenizer,
+            "pad_tok_id": pad_tok_id,
+            "prefix_string": prefix_string,
+            "chosen": chosen,
+            "question": question,
+            "choices": choices,
+            "answer": answer,
+            "device": device
+        }
+        model_answer = helper_function(**kwargs)
+        if model_answer is None:
+            continue
+
+        # Convert into tensor
+        model_answer = torch.tensor(model_answer, dtype=torch.long, device=device)
+        final_result.append(1 if torch.argmax(model_answer) == answer else 0)
+
+    num_correct = sum(final_result)
+    print(f"Total Questions: {len(final_result)}")
+    print(f"Number of answers correct: {num_correct}")
+
+    print(f"Accuracy: {num_correct/len(final_result)}")
 
 
 @torch.no_grad()
-def hellaswag_eval():
-    pass
+def arc_e_eval(model: LLaMaTransformer, tokenizer: Tokenizer, max_chars: int, pad_tok_id: int):
+    arc_easy = load_dataset("ai2_arc", "ARC-Easy")
 
 
+@torch.no_grad()
+def hellaswag_eval(model: LLaMaTransformer, tokenizer: Tokenizer, max_chars: int, pad_tok_id: int):
+    hellaswag = load_dataset("Rowan/hellaswag")
+
+
+@torch.no_grad()
+def piqa_eval(model: LLaMaTransformer, tokenizer: Tokenizer, max_chars: int, pad_tok_id: int):
+    piqa = load_dataset("piqa")
 
 
 
@@ -222,7 +326,7 @@ if __name__ == "__main__":
     print(f"Now using {device=}")
 
     # Specify the 'model' path (technically checkpoint) and state dict
-    model_path = root_path("simple_llama", "pretraining", "checkpoints", "model_300M_4278L_2048MSQ.pth")
+    model_path = root_path("simple_llama", "pretraining", "checkpoints", "model_500M_3744L_2048MSQ.pth")
     ckpt = torch.load(model_path, map_location=device, weights_only=False)
 
     # Load in pretrained tokenizer. Don't really need to decode here, just need raw logits
@@ -252,4 +356,6 @@ if __name__ == "__main__":
 
     # Generally either 2048 or 4096 for max tokens
     max_chars = int(ckpt["config"].max_seq_len * 3.5)  # Usually it's around 4 chars per token, 3.5 for a bit of room
+    # arc_c_eval(model=model, tokenizer=tokenizer, max_chars=max_chars, pad_tok_id=pad_tok_id)
+    # exit()
     mmlu_eval(model=model, tokenizer=tokenizer, max_chars=max_chars, pad_tok_id=pad_tok_id)
